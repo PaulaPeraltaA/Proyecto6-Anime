3353b9aa0cda4eeaeb32be674397d036dc7142e0
PaulaPeraltaA <paupagui@espol.edu.ec>
2023-07-19 14:05:07 -0500

    cambio guia 16.2

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+//Importación de la interfaz
+import { Root2 } from '../interface/root';
+
+//Importación del servicio
+import { RootService } from '../providers/root.service';
+
 
 @Component({
   selector: 'app-services',
@@ -6,5 +12,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./services.component.css']
 })
 export class ServicesComponent {
+  constructor(private dataProvider: RootService) { }
 
-}
+  //Atributo con el tipo de dato de la interfaz
+  public data : Root2[] = [];
+
+  ngOnInit() {
+    this.dataProvider.getResponse().subscribe((response) => { 
+      this.data = (response as Root2[]); 
+    })
+  }
+
+  }
+
